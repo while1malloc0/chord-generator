8ad46712ae2f42fb5567ae611e402a93c0d480e2
John Turner <while1malloc0@gmail.com>
2021-06-18 19:12:20 -0400

    Move chord gen to its own class

---
+++ chord_generator.py
@@ -0,0 +1,55 @@
+from random import randint
+
+# Lay these out in the circle of fifths so that that logic is easier
+notes = ["C", "F", "Bb", "Eb", "Ab", "Db", "F#", "B", "E", "A", "D", "G"]
+
+# TODO [jturner 2021-06-17]: this approach is kind of naive, but it works. For
+# more interesting combinations, this would use something like a Markov chain
+# so that you could express certain stacks of "second order" extensions (e.g. a
+# sus4 with a 9) without having to do annoying filtering for each new extension
+# (e.g. "we've added a sus4, so now 6, 9 and 13 are fine but the 'altered'
+# extension aren't anymore")
+qualities_with_extensions = {
+    "maj": ["6", "7", "6/9", "7/9", "7#11", "7#5", "7/13"],
+    "min": ["6", "7", "6/9", "7/9", "7/11"],
+    "7": ["#11", "b5", "#5", "/9", "b9", "#9", "b13", "/13", "sus4", "sus2"],
+    "min7b5": [],
+    "dim7": [],
+}
+
+
+def next_note_cof():
+    """picks the next note in the circle of fifths"""
+    while True:
+        for i in range(0, len(notes)):
+            yield i
+
+
+def next_note_rand():
+    """picks the next note at random"""
+    while True:
+        yield randint(0, len(notes) - 1)
+
+
+class ChordGenerator:
+    def __init__(self, note_picker, allow_extensions, qualities):
+        self.note_picker = note_picker()
+        self.allow_extensions = allow_extensions
+        self.qualities = qualities
+
+    def get_chord(self):
+        which_quality = randint(0, len(self.qualities) - 1)
+        quality = self.qualities[which_quality]
+
+        extension = ""
+        if self.allow_extensions:
+            possible_extensions = qualities_with_extensions[quality]
+            if possible_extensions:
+                which_extension = randint(0, len(possible_extensions) - 1)
+                extension = possible_extensions[which_extension]
+
+        which_note = next(self.note_picker)
+        note = notes[which_note]
+
+        chord = f"{note}{quality}{extension}"
+        return chord
--- gen_chords.py
+++ gen_chords.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
 
+from chord_generator import ChordGenerator, next_note_cof, next_note_rand
 from random import randint, seed
 from datetime import datetime
 import argparse
 
-
-# Lay these out in the circle of fifths so that that logic is easier
-notes = ["C", "F", "Bb", "Eb", "Ab", "Db", "F#", "B", "E", "A", "D", "G"]
-
-
-# TODO [jturner 2021-06-17]: this approach is kind of naive, but it works. For
-# more interesting combinations, this would use something like a Markov chain
-# so that you could express certain stacks of "second order" extensions (e.g. a
-# sus4 with a 9) without having to do annoying filtering for each new extension
-# (e.g. "we've added a sus4, so now 6, 9 and 13 are fine but the 'altered'
-# extension aren't anymore")
-qualities_with_extensions = {
-    "maj": ["6", "7", "6/9", "7/9", "7#11", "7#5", "7/13"],
-    "min": ["6", "7", "6/9", "7/9", "7/11"],
-    "7": ["#11", "b5", "#5", "/9", "b9", "#9", "b13", "/13", "sus4", "sus2"],
-    "min7b5": [],
-    "dim7": [],
-}
 
 usage = """
 Usage: random_chords.py NUM_CHORDS
@@ -29,20 +12,6 @@ Usage: random_chords.py NUM_CHORDS
 Args:
   NUM_CHORDS - How many chords to generate
 """
-
-
-def next_note_cof():
-    """picks the next note in the circle of fifths"""
-    while True:
-        for i in range(0, len(notes)):
-            yield i
-
-
-def next_note_rand():
-    """picks the next note at random"""
-    while True:
-        yield randint(0, len(notes) - 1)
-
 
 parser = argparse.ArgumentParser(
     description="Generate random chords for practice purposes"
@@ -80,10 +49,9 @@ if __name__ == "__main__":
     seed(datetime.now())
 
     # get our iterator for picking the next note, default to random
-    get_next_note_picker = next_note_rand
+    next_note_picker = next_note_rand
     if args.order == "cof":
-        get_next_note_picker = next_note_cof
-    next_note_picker = get_next_note_picker()
+        next_note_picker = next_note_cof
 
     # get the chord qualities that we're allowed to choose from, default to all
     available_qualities = args.qualities
@@ -95,25 +63,15 @@ if __name__ == "__main__":
     # are allowed, as with qualities
     extensions_allowed = not hasattr(args, "no_extensions")
 
+    chord_gen = ChordGenerator(
+        next_note_picker, extensions_allowed, available_qualities
+    )
+
     # assemble a list of N chords, which are note + quality + extensions,
     # e.g. A + min + 6/9 = Amin6/9
     chords = []
     for i in range(0, args.num_chords):
-        which_quality = randint(0, len(available_qualities) - 1)
-        quality = available_qualities[which_quality]
-
-        extension = ""
-        if extensions_allowed:
-            possible_extensions = qualities_with_extensions[quality]
-            if possible_extensions:
-                which_extension = randint(0, len(possible_extensions) - 1)
-                extension = possible_extensions[which_extension]
-
-        which_note = next(next_note_picker)
-        note = notes[which_note]
-
-        chord = f"{note}{quality}{extension}"
-        chords.append(chord)
+        chords.append(chord_gen.get_chord())
 
     # Pretty print the chords
     # TODO [jturner 2021-06-17]: use a real table writer for this
