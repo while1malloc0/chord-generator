0204f340bcddce8fbf97a748ed1b6b38fd1a9028
John Turner <while1malloc0@gmail.com>
2021-06-17 23:04:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Chord Generator
+
+A small utility to generate random chords for exercises
+
+## Usage
+
+random_chords.py NUM_CHORDS
+
+Args:
+  
+- NUM_CHORDS - the number of chords to generate
---
+++ random_chords.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from random import randint, seed
+from sys import argv
+from datetime import datetime
+import os
+
+
+notes = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G"
+]
+
+
+qualities_with_extensions = {
+    "maj": ["6", "7", "6/9", "7/9", "7#11", "7#5", "7/13"],
+    "min": ["6", "7", "6/9", "7/9", "7/11"],
+    "7": ["#11", "b5", "#5", "/9", "b9", "#9", "b13", "/13", "sus4", "sus2"],
+    "min7b5": [],
+    "dim7": [],
+}
+
+usage = """
+Usage: random_chords.py NUM_CHORDS
+
+Args:
+  NUM_CHORDS - How many chords to generate
+"""
+
+    
+
+if __name__ == '__main__':
+    if len(argv) < 1:
+        print(usage)
+        os.exit(0)
+
+    seed(datetime.now())
+    num_chords = int(argv[1])
+
+    chords = []
+    for i in range(0, num_chords):
+        quality_index = randint(0, len(qualities_with_extensions)-1)
+        quality = list(qualities_with_extensions.keys())[quality_index]
+        possible_extensions = qualities_with_extensions[quality]
+        extension = ""
+        if possible_extensions:
+            extension_index = randint(0, len(possible_extensions)-1)
+            extension = possible_extensions[extension_index]
+        note = notes[randint(0, len(notes)-1)]
+        chord = f"{note}{quality}{extension}"
+        chords.append(chord)
+
+    s = ""
+    for i, chord in enumerate(chords):
+        if i % 4 == 0 and s != "":
+            s = "|" + s
+            print(s)
+            s = ""
+        s += " {:<8} |".format(chord)
+        
